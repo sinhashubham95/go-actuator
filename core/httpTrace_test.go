3df77fc98bb9c5de5b9f2806eccf6c155ae7f41b
s0s01qp <shubham.sinha@walmartlabs.com>
2021-07-21 09:33:06 +0530

    added test cases

--- core/httpTrace_test.go
+++ core/httpTrace_test.go
@@ -2,6 +2,7 @@ package core_test
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/sinhashubham95/go-actuator/commons"
 	"github.com/stretchr/testify/assert"
 	"github.com/valyala/fasthttp"
 	"net/http"
@@ -13,10 +14,6 @@ import (
 
 func TestGetHTTPTrace(t *testing.T) {
 	assert.Empty(t, core.GetHTTPTrace())
-}
-
-func TestWithFastHTTP(t *testing.T) {
-
 }
 
 func TestWithGIN(t *testing.T) {
@@ -38,7 +35,42 @@ func TestWithGIN(t *testing.T) {
 }
 
 func TestWithNetHTTP(t *testing.T) {
+	m := &http.ServeMux{}
+	m.HandleFunc("/ping", getNetHTTPHandler())
+	w := httptest.NewRecorder()
+	request, err := http.NewRequest(http.MethodGet, "/ping", nil)
+	assert.NoError(t, err)
+	m.ServeHTTP(w, request)
+	assert.Equal(t, http.StatusOK, w.Code)
 
+	traces := core.GetHTTPTrace()
+	assert.NotEmpty(t, traces)
+	trace := traces[0]
+	assert.Equal(t, http.MethodGet, trace.Request.Method)
+	assert.Equal(t, "/ping", trace.Request.URL)
+	assert.Empty(t, trace.Request.Headers)
+	assert.Equal(t, http.StatusOK, trace.Response.Status)
+	assert.Empty(t, trace.Response.Headers)
+}
+
+func TestForMoreThanThresholdRequests(t *testing.T) {
+	router := setupGINRouter()
+	for i := 0; i <= (commons.HTTPTraceResultsSize + 5); i += 1 {
+		w := httptest.NewRecorder()
+		request, err := http.NewRequest(http.MethodGet, "/ping", nil)
+		assert.NoError(t, err)
+		router.ServeHTTP(w, request)
+		assert.Equal(t, http.StatusOK, w.Code)
+	}
+
+	traces := core.GetHTTPTrace()
+	assert.Equal(t, commons.HTTPTraceResultsSize, len(traces))
+	trace := traces[0]
+	assert.Equal(t, http.MethodGet, trace.Request.Method)
+	assert.Equal(t, "/ping", trace.Request.URL)
+	assert.Empty(t, trace.Request.Headers)
+	assert.Equal(t, http.StatusOK, trace.Response.Status)
+	assert.Empty(t, trace.Response.Headers)
 }
 
 func getFastHTTPHandler() fasthttp.RequestHandler {
@@ -54,4 +86,10 @@ func setupGINRouter() *gin.Engine {
 		ctx.Status(http.StatusOK)
 	})
 	return router
+}
+
+func getNetHTTPHandler() http.HandlerFunc {
+	return core.WrapNetHTTPHandler(func(writer http.ResponseWriter, request *http.Request) {
+		writer.WriteHeader(http.StatusOK)
+	})
 }
